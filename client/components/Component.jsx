c99877f21d882f5220379dbc37744ecbba4276f7
Angel <yhosy_soldier@hotmail.com>
2018-02-15 15:05:35 +0100

    proyecto actualizado

--- client/components/Component.jsx
+++ client/components/Component.jsx
@@ -1,35 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 const Component = React.createClass({
+
+	getInitialState: function () {
+		console.log("Estoy en el initial state...");
+		return { vida: 20 };
+
+	},
+
+	sumar_vida() {
+
+		let vida=++this.state.vida;
+		this.setState({ vida: vida });
+
+	},
+
+	restar_vida() {
+		let vida=--this.state.vida;
+		this.setState({ vida: vida });
+	},
+	
 	render() {
-		
-		let vida = this.props.vida;
-		
-		function sumar_vida(){
-			vida++;
-		}
-
-		function restar_vida(){
-			vida--;
-		}
-
 		return (
 			<div>
 				<h2>Jugador 1</h2>
 				<div>
-					<button onClick={sumar_vida}>+</button>
-					<label htmlFor="">{vida}</label>
-					<button onClick={restar_vida}>-</button>
+
+					<button onClick={this.sumar_vida}>+</button>
+					<label htmlFor="">{this.state.vida}</label>
+					<button onClick={this.restar_vida}>-</button>
+
 				</div>
 			</div>
 		)
 	}
 })
-
-// React.render(
-//     <Component/>,
-//     document.getElementById('root')
-// );
-
 
 export default Component;
